0db909ad1c8a5f9f392ca5ba7e5f898c23dbc1cf
Ivan Demenkov <ivan@demenkov.com>
2016-03-29 20:49:38 +0200

    Update SassScriptLexer.php

--- script/SassScriptLexer.php
+++ script/SassScriptLexer.php
@@ -67,7 +67,7 @@ class SassScriptLexer
     }
 	$tokens = array();
     // whilst the string is not empty, split it into it's tokens.
-    while (intval(substr($string, 0) == "")) {
+    while ($string !== false) {
       if (($match = $this->isWhitespace($string)) !== false) {
         $tokens[] = null;
       } elseif (($match = SassScriptFunction::isa($string)) !== false) {
